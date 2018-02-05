cceae8726cc3519c0512491f0ebfbb27c4c89204
Joshua Murphy <joshuatmurphy@gmail.com>
2018-02-04 20:04:37 -0500

    Permutation should read number of items from first command-line argument

--- src/main/java/Permutation.java
+++ src/main/java/Permutation.java
@@ -3,7 +3,7 @@ import edu.princeton.cs.algs4.StdIn;
 public class Permutation {
     public static void main(String[] args) {
         RandomizedQueue<String> queue = new RandomizedQueue<>();
-        int k = StdIn.readInt();
+        int k = Integer.parseInt(args[0]);
         while (!StdIn.isEmpty()) {
             queue.enqueue(StdIn.readString());
         }
