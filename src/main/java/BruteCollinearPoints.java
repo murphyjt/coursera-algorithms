21c96269afe088b7679c7348ea7bf8b577aee6f8
Joshua Murphy <joshuatmurphy@gmail.com>
2018-02-11 14:25:53 -0500

    Use supplied main() for BruteCollinearPoints

--- src/main/java/BruteCollinearPoints.java
+++ src/main/java/BruteCollinearPoints.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 
-import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
 public class BruteCollinearPoints {
 
@@ -85,19 +87,33 @@ public class BruteCollinearPoints {
     }
 
     public static void main(String[] args) {
-        int n = 0;
-        Point[] points = new Point[StdIn.readInt()];
-        while (!StdIn.isEmpty()) {
-            int x = StdIn.readInt();
-            int y = StdIn.readInt();
-            points[n++] = new Point(x, y);
-        }
 
-        BruteCollinearPoints collinearPoints = new BruteCollinearPoints(points);
-        System.out.println(collinearPoints.numberOfSegments());
-        for (LineSegment lineSegment : collinearPoints.lineSegments) {
-            System.out.println(lineSegment);
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
         }
+    
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+    
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
     }
 
 }
